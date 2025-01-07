8374a04235029b752c62ca98686ae476b68da31c
Ethano1998 <ethan.oualid@campus.technion.ac.il>
2025-01-07 11:01:50 +0200

    globaltable

--- symbolTable.hpp
+++ symbolTable.hpp
@@ -29,23 +29,65 @@ public:
 
 class SymbolTable {
     int offset = 0;
-    
-    public:
     std::vector<std::shared_ptr<SymbolEntry>> entries;
+
+    public:
     SymbolTable() = default;
-    void addEntry(std::shared_ptr<SymbolEntry> entry) {
-        entries.push_back(entry);
-    }
+    
     std::shared_ptr<SymbolEntry> findEntry(std::string name) {
         for (auto entry : entries) {
-            if (dynamic_cast<VarSymbolEntry*>(entry.get())) {
-                if (dynamic_cast<VarSymbolEntry*>(entry.get())->name == name) {
+            if (std::dynamic_pointer_cast<VarSymbolEntry>(entry)) {
+                if (std::dynamic_pointer_cast<VarSymbolEntry>(entry)->name == name) {
                     return entry;
                 }
-            } else if (dynamic_cast<FuncSymbolEntry*>(entry.get())) {
-                if (dynamic_cast<FuncSymbolEntry*>(entry.get())->name == name) {
+            } else if (std::dynamic_pointer_cast<FuncSymbolEntry>(entry)) {
+                if (std::dynamic_pointer_cast<FuncSymbolEntry>(entry)->name == name) {
                     return entry;
                 }
+            }
+        }
+        return nullptr;
+    }
+
+    void addVar(std::string name, std::string type) {
+        entries.push_back(std::make_shared<VarSymbolEntry>(name, type, offset));
+        offset++;
+    }
+
+    void addFunc(std::string name, std::vector<std::string> paramTypes, std::string returnType) {
+        entries.push_back(std::make_shared<FuncSymbolEntry>(name, paramTypes, returnType));
+    }
+};
+
+class GlobalSymbolTable {
+    std::vector<std::shared_ptr<SymbolTable>> tables;
+    public:
+    GlobalSymbolTable(){
+        std::shared_ptr<SymbolTable> table = std::make_shared<SymbolTable>();
+        table->addFunc("print", std::vector<std::string>{"string"}, "void");
+        table->addFunc("printi", std::vector<std::string>{"int"}, "void");
+        tables.push_back(table);
+    };
+
+    void addTable(std::shared_ptr<SymbolTable> table) {
+        tables.push_back(table);
+    }
+
+    std::shared_ptr<SymbolTable> getTable() {
+        return tables.back();
+    }
+
+    std::shared_ptr<SymbolTable> popTable() {
+        auto table = tables.back();
+        tables.pop_back();
+        return table;
+    }
+
+    std::shared_ptr<SymbolEntry> findEntry(std::string name) {
+        for (auto table : tables) {
+            auto entry = table->findEntry(name);
+            if (entry) {
+                return entry;
             }
         }
         return nullptr;
