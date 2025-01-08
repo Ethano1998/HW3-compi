6cc8f8fab40dcf9e982f96711b6fd44e38efc857
Ethano1998 <ethan.oualid@campus.technion.ac.il>
2025-01-08 22:12:17 +0200

    return

--- SemanticVisitor.cpp
+++ SemanticVisitor.cpp
@@ -255,13 +255,13 @@ void SemanticVisitor::visit(ast::Call &node){
 
 void SemanticVisitor::visit(ast::Return &node){
     if(!node.exp){
-        if(node != toString(ast::BuiltInType::VOID))
+        if(node.return_type != ast::BuiltInType::VOID){
             output::errorMismatch(node.line);
-        ast::BuiltInType type_check = check_assign(node.exp);
-        std::string check = toString(type_check);
-        if(check != toString(node.type))
-            output::errorMismatch(node.line);
+        }
     }
-
-    
+    else{
+        if(node.return_type != check_assign(node.exp) || node.return_type == ast::BuiltInType::VOID){
+            output::errorMismatch(node.line);
+        }
+    }
 }
