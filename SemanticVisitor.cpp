00c21fb4cf2a45ad4e1fe63ebd1a093f685cb463
elie daniel tapiero <elie.tapiero@campus.technion.ac.il>
2025-01-09 19:00:28 +0200

    condition if mismatch

--- SemanticVisitor.cpp
+++ SemanticVisitor.cpp
@@ -234,7 +234,7 @@ void SemanticVisitor::visit(ast::If &node){
     globalSymbolTable.addTable(table);
     node.condition->accept(*this);  //need to check if there is no problem with the condition
     if(node.condition->type !=  ast::BuiltInType::BOOL){
-        output::errorMismatch(node.line);
+        output::errorMismatch(node.condition->line);
     }
     node.then->return_type = node.return_type;
     node.then->accept(*this);
